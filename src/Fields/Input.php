4cf68729cdde27015b3259b2967fcfeb70bd0bd8
Ric Le Poidevin <ric@wearebwi.com>
2020-10-09 14:09:24 +0100

    Field

---
+++ src/Fields/Input.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Riclep\StoryblokForms\Fields;
+
+
+use Riclep\Storyblok\Field;
+
+class Input extends Field
+{
+	public function __toString()
+	{
+		// TODO: Implement __toString() method.
+	}
+}

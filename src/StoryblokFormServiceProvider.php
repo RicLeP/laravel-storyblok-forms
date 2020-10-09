0a4b43dda436a4b72c3a112512c24474decac33a
Ric Le Poidevin <ric@wearebwi.com>
2020-10-09 16:51:00 +0100

    Service provider

---
+++ src/StoryblokFormServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Riclep\Storyblok;
+
+use Illuminate\Support\ServiceProvider;
+use Riclep\Storyblok\Console\BlockMakeCommand;
+use Storyblok\Client;
+
+class StoryblokFormServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+
+    }
+}

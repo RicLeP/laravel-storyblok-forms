5332d7c6ed1b11c3112c2c7b93dc5152b2af0865
Richard Le Poidevin <ric@sirric.co.uk>
2022-06-17 17:20:16 +0100

    WIP - Conditional fields responses contain sub fields

--- src/Blocks/LsfConditionalSelect.php
+++ src/Blocks/LsfConditionalSelect.php
@@ -12,4 +12,59 @@ class LsfConditionalSelect extends MultiInput
 	protected $optionsName = 'options';
 
 	protected $type = 'multi-input';
+
+
+
+
+	/**
+	 * Returns all the validation rules for the fields in this Fieldset
+	 *
+	 * @return array
+	 */
+	public function validationRules() {
+		$rules = [];
+
+		$this->fields->each(function ($field) use (&$rules) {
+			$rules = array_merge($rules, $field->validationRules());
+		});
+
+		return $rules;
+	}
+
+	/**
+	 * Returns all the error messages for the fields in this Fieldset
+	 *
+	 * @return array
+	 */
+	public function errorMessages() {
+		$rules = [];
+
+		$this->fields->each(function ($field) use (&$rules) {
+			$rules = array_merge($rules, $field->errorMessages());
+		});
+
+		return $rules;
+	}
+
+
+
+	// TODO - store the selected input value
+	// TODO - only respond with conditionally on inputs
+
+	public function response($input) {
+		return [
+			'label' => $this->label,
+			'response' => $this->fields->map(function ($field) use ($input) {
+
+				// Handle empty radio buttons etc. sending nothing in POST request
+				if (!array_key_exists($field->name, $input)) {
+					$input[$field->name] = null;
+				}
+
+
+				return $field->response($input[$field->name]);
+			})->toArray(),
+			'type' => $this->type,
+		];
+	}
 }

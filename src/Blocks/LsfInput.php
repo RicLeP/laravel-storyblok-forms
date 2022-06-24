d763585eab8d85898275821cb525722d1224906d
Richard Le Poidevin <ric@sirric.co.uk>
2022-06-24 12:56:14 +0100

    Rewrite required validation rules in conditional fieldsets to required_if

--- src/Blocks/LsfInput.php
+++ src/Blocks/LsfInput.php
@@ -11,6 +11,36 @@ class LsfInput extends Input
 	protected $type = 'input';
 
 	/**
+	 * All the Validation rules for this Input
+	 *
+	 * @return mixed
+	 */
+	public function validationRules() {
+		$rules = $this->validators->validationRules();
+
+		/**
+		 * Rewrite required to required_if for items inside conditional selects
+		 */
+		if ($this->parent() instanceof LsfConditionalSelect) {
+			if (is_array($this->settings('lsf_is_conditional')['when_parent_is'])) {
+				$requiredWhen = implode(',', $this->settings('lsf_is_conditional')['when_parent_is']);
+			} else {
+				$requiredWhen = $this->settings('lsf_is_conditional')['when_parent_is'];
+			}
+
+			foreach ($rules as $key => $rule) {
+				if (in_array('required', $rule)) {
+					$requiredKey = array_search('required', $rule);
+
+					$rules[$key][$requiredKey] = 'required_if:' . $this->parent()->name . ',' . $requiredWhen;
+				}
+			}
+		}
+
+		return $rules;
+	}
+
+	/**
 	 * Returns the Input’s response after the form has been submitted and validated
 	 *
 	 * @param $input

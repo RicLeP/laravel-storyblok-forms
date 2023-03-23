1fb4ab69c75a91987fed42814599f7ddaf32204e
Richard Le Poidevin <ric@sirric.co.uk>
2023-03-23 10:49:53 +0000

    Added not_one_of condition

--- src/Rules/ConditionallyRequired.php
+++ src/Rules/ConditionallyRequired.php
@@ -61,6 +61,8 @@ class ConditionallyRequired implements DataAwareRule, InvokableRule
 				$fieldIsRequired = $causationFieldValue <= $condition;
 			} else if ($operator === 'one_of') {
 				$fieldIsRequired = in_array((int) $causationFieldValue[0], $condition); // always array?
+			} else if ($operator === 'not_one_of') {
+				$fieldIsRequired = !in_array((int) $causationFieldValue[0], $condition); // always array?
 			}
 
 			if ($fieldIsRequired && !$value) {
